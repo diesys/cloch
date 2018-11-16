e82390d079c082674273e97ebf51564350bf9220
sbiego <forsesim@anche.no>
2018-11-16 20:00:09 +0100

    better color animation minutes, cleaned some code

--- js/cloch.js
+++ js/cloch.js
@@ -81,95 +81,6 @@ function setHour(new_hour, style='fade') {
     config['hour'] = hour;
 }
 
-function setMinute2(min, lazy = true) {
-    if (min < 60) {
-        // var minOpacity = lazy ? minutesStateLazy : minutesState;
-        // console.log('lazy = ', lazy, '\nopacity levels:', minOpacity);
-
-        // a minute is about 0.2, toFixed gives 1 dec digit,tmp to string,separate dec from floor
-        var tmp = ((min / 5) % 12).toFixed(1).toString().split('.'),
-
-            // to int and normalize newMin[1] for index opacity ranges [0..3] levels
-            newMin = {'value': min};
-            newMin['indicator_index'] = parseInt(tmp[0]);
-            newMin['opacity_index'] = tmp[1] / 2;
-            // console.log('minopacity:', newMin['opacity_index']);
-            config['minute'] = newMin;
-        
-        if (min == 0)
-            var prec = minutes[11];
-        else
-            var prec = minutes[(newMin['indicator_index'] - 1) % 12];
-
-        var first = minutes[newMin['indicator_index'] % 12],
-            second = minutes[(newMin['indicator_index'] + 1) % 12];
-
-        // starts animation with fade-in consistent opacity level and leaves the indicator near the full opacity one in exact 10n and idle when in middle position(5n min)
-
-        if (newMin['value'] % 10 == 0) {
-            TweenMax.to(prec, 7 * durationFade, {
-                fill: 'rgba(255,255,255,0)',
-            });
-            TweenMax.to(second, 7 * durationFade, {
-                fill: 'rgba(255,255,255,0)',
-            });
-            console.log('mod 10');
-        } else if (newMin['value'] % 5 == 0 && newMin['value'] % 10 != 0) {
-            TweenMax.to(prec, 7 * durationFade, {
-                fill: 'rgba(0,0,0,.1)',
-            });
-            TweenMax.to(second, 7 * durationFade, {
-                fill: 'rgba(0,0,0,.1)',
-            });
-            console.log('mod 5');
-        } else {
-            // TweenMax.from(prec, 7 * durationFade, {
-            //     fill: 'rgba(255,255,255,' + minutesState['first_start'][newMin['opacity_index']] + ')',
-            // });
-            TweenMax.from(second, 7 * durationFade, {
-                fill: 'rgba(255,255,255,' + minutesState['second_start'][newMin['opacity_index']] + ')',
-            });
-            TweenMax.to(second, 7 * durationFade,{
-                fill: 'rgba(255,255,255,' + minutesState['second'][newMin['opacity_index']] + ')',
-                delay: 2 * durationFade,
-            });
-        }
-
-        TweenMax.from(first, 7 * durationFade, {
-            fill: 'rgba(255,255,255,' + minutesState['first_start'][newMin['opacity_index']] + ')',
-        });
-        TweenMax.to(first, 7 * durationFade,{
-            fill: 'rgba(255,255,255,' + minutesState['first'][newMin['opacity_index']] + ')',
-            delay: 2 * durationFade,    
-        });
-        
-        
-        // remove bg from other indicators
-        for (i = 0; i < 12; i++)
-        if (i != newMin['indicator_index'])
-            if(i % 2 == 0) {
-                TweenMax.to(minutes[i], 10 * durationFade,{
-                    fill: 'rgba(0,0,0,.1)',
-                    delay: 0,
-                });
-            }
-            else {
-                TweenMax.to(minutes[i], 10 * durationFade,{
-                    delay: 0,
-                    fill: 'rgba(0,0,0,.05)',
-                });
-            }
-        // minutes[i].css({
-                //     'fill': 'red'
-                // });
-                
-
-        // console.log(newMin);
-
-    } else console.log('error: minutes can be [0..60]');
-}
-
-
 function setMinute(min, lazy = true) {
     // sembra che sia trasparente il successivo(second) al primo passo del nuovo first DA RIVEDERE
 
@@ -187,8 +98,10 @@ function setMinute(min, lazy = true) {
                 'opacity_index': tmp[1] / 2
             };
         
+        // adding the minute to the conf
         config['minute'] = newMin;
 
+        // 
         if (min == 0)
             var prec = minutes[11];
         else
@@ -196,74 +109,45 @@ function setMinute(min, lazy = true) {
 
         var first = minutes[newMin['indicator_index'] % 12],
             second = minutes[(newMin['indicator_index'] + 1) % 12];
+       
 
-        TweenMax.set(first, {
-            fill: 'blue',
-        });
-        TweenMax.set(second, {
-            fill: 'blue',
-        });
-        
-        TweenMax.fromTo(first, 7 * durationFade, {
-                fill: 'rgba(255,255,255,' + minutesState['first_start'][newMin['opacity_index']] + ')',
-                delay: 2 * durationFade,
-            },{
-                fill: 'rgba(255,255,255,' + minutesState['first'][newMin['opacity_index']] + ')',
-                delay: 2 * durationFade,
-            });
-            TweenMax.fromTo(second, 7 * durationFade, {
-                fill: 'rgba(255,255,255,' + minutesState['second_start'][newMin['opacity_index']] + ')',
+        // indicators' animations
+        TweenMax.fromTo(first, 8 * durationFade, {
+                fill: minutesColors['first_start'][newMin['opacity_index']],
                 delay: 2 * durationFade,
             }, {
-                fill: 'rgba(255,255,255,' + minutesState['second'][newMin['opacity_index']] + ')',
+                fill: minutesColors['first'][newMin['opacity_index']],
                 delay: 2 * durationFade,
         });
-        
+
+        TweenMax.fromTo(second, 8 * durationFade, {
+                fill: minutesColors['second_start'][newMin['opacity_index']],
+                delay: 2 * durationFade,
+            }, {
+                fill: minutesColors['second'][newMin['opacity_index']],
+                delay: 2 * durationFade,
+        });
 
 
         // remove bg from other indicators
         for (i = 0; i < 12; i++)
             if (i != newMin['indicator_index'])
                 if (i % 2 == 0) {
-                    TweenMax.to(minutes[i], 10 * durationFade, {
+                    TweenMax.to(minutes[i], 8 * durationFade, {
                         fill: 'rgba(0,0,0,.1)',
                         delay: 0,
                     });
                 }
         else {
-            TweenMax.to(minutes[i], 10 * durationFade, {
+            TweenMax.to(minutes[i], 8 * durationFade, {
                 delay: 0,
                 fill: 'rgba(0,0,0,.05)',
             });
         }
-        // minutes[i].css({
-        //     'fill': 'red'
-        // });
-
-
+       
         // console.log(newMin);
 
     } else console.log('error: minutes can be [0..60]');
-}
-
-
-
-function updateCloch(hour, minute) {
-    sunMoon[0].animate({
-        'opacity': '320'
-    }, {
-        step: function (now, fx) {
-            $(this).css({
-                "transform": "translate(0px, " + now + "px, 0px)"
-            });
-        },
-        duration: 10000,
-        easing: 'linear',
-        queue: false,
-        complete: function () {
-            alert('Animation is done');
-        }
-    }, 'linear');
 }
 
 // function toggle_colorpicker() {
@@ -277,12 +161,14 @@ function updateCloch(hour, minute) {
 // }
 
 window.onload = function () {
+    // main cloch elements
     cloch = $('#cloch'),
+    quadrant = $('#quadranthx'),
+    background = $('#backgroundhx');
+    
     // shows the cloch
     cloch.css({'opacity': '1'});
     
-    quadrant = $('#quadranthx'),
-    background = $('#backgroundhx'),
     
     // MINUTES ANGLE: 0, 10, 20, 30, 50, 60  in clockwise order
     // MINUTES MIDDLE: 5, 15, 25, 35, 45, 55 in clockwise order
@@ -297,82 +183,55 @@ window.onload = function () {
     // Hours traslation coordinates
     hoursXY = new Array([-33.8, -19], [0, 0], [0, 40], [-33.8, 58], [-67, 38], [-67, 0]);
 
-    // Minutes opacity values 
-       // es 5min, 2 'triangles' indicators (angle and middle) with 3 states 
-       // [0]: A=100, 
-       // [1]: [A=100, M=40],
-       // [2]: [A=70, M=40],
-       // [3]: [A=40, M=70],
-       // [4]: [A=40, M=100],
-       // restart [5]: M=100, ...
-    minutesState = {
-        'first': [1,1,.7,.4,.4], 
-        'second': [.1,.4,.4,.7,1],
-        // used for animation transition between minutes
-        'first_start': [1,1,1,.7,.4], 
-        'second_start': [0,0,.4,.4,.7]
-    };
-    
     // general duration fade used for relative timing and delay
     durationFade = .1,
     
     // global variable for start/stopping the clock for manual select
     stopCloch = false;
 
-    config = getUrlVars();
-    
-    
-    // main
-    
-    // sets sun in the center
-    TweenMax.set(sunMoon[0], {
-        x:-33.8, y:20,
-        transformOrigin: "-33px 20px",
-    });
-
+    // Minutes indicator color values 
+    minutesColors = {
+        // from < animation
+        'first_start': ['rgba(255,255,255,1)', 'rgba(255,255,255,1)', 'rgba(255,255,255,1)', 'rgba(255, 255, 255, .7)', 'rgba(255, 255, 255, .4)'], 
+        'second_start': ['rgba(0,0,0,.1)', 'rgba(255,255,255,.1)', 'rgba(255,255,255,.4)', 'rgba(255, 255, 255, .4)', 'rgba(255, 255, 255, .7)'], 
         
+        // to > animation
+        'first': ['rgba(255,255,255,1)', 'rgba(255,255,255,1)', 'rgba(255,255,255,.7)', 'rgba(255, 255, 255, .4)', 'rgba(255, 255, 255, .4)'], 
+        'second': ['rgba(0,0,0,.1)', 'rgba(255,255,255,.4)', 'rgba(255,255,255,.4)', 'rgba(255, 255, 255, .7)', 'rgba(255, 255, 255, 1)'], 
+    };
+
+    
+    ////// MAIN ///////////   
+    
+    // get url color if any
+    config = getUrlVars();
     if (!config['color'])
         config['color'] = '#ff3c6d';
-    changeColor(config['color'],0);
+    changeColor(config['color'], 0);
 
+    // gets new date starts adding to config file 
     date = new Date();
     config['hour'] = {'value': date.getHours()};
     config['minute'] = {'value': date.getMinutes()};
-    // setHour(config['currHour'], 'fade');
-    
-    first_minute = true;
     setTimeout(function () {
-        date = new Date();
         setHour(date.getHours());
         setMinute(date.getMinutes());
         // console.log('first AUTO cloch configuration: ', config);
-        // console.log('seconds2:', 60 - date.getSeconds());
     }, 0);
     
+    // checks if there's any hour and minute in the url
     setInterval(function () {
-        if(!stopCloch) {
+        if (!stopCloch) {
             date = new Date();
             h = date.getHours();
             m = date.getMinutes();
-            // m = date.getSeconds();
             if (h != config['hour']['value'])
                 setHour(h);
             if (m != config['minute']['value'])
                 setMinute(m);
-            
         }
-            // console.log('AUTO cloch configuration: ', config, 'seconds:', first_minute ? ((60 - date.getSeconds()) * 1000) : 60000);
-        // }, 1000); //troppo poco??
-        }, 500);//500); //troppo poco??
-
-    
-    // }, first_minute ? ((60 - date.getSeconds()) * 1000) : 60000); // millisecondi sono i rimanenti rispetto al secondo attuale?
-    // console.log('B', first_minute);
-    first_minute = false;
-    // console.log('A', first_minute);
-
-    
-
+        // console.log('AUTO cloch configuration: ', config, 'seconds:', first_minute ? ((60 - date.getSeconds()) * 1000) : 60000);
+    }, 500);
 
 
     ////////// TO - DO //////////////////
@@ -391,7 +250,13 @@ window.onload = function () {
     
     //rivedere funzione aggiorna ora che cancella le altre allo stato iniziale se non servono
 
+    // ora manuale, nelle select, automaticamente settata su quella attuale
 
+    // implementare ora nell'url
+
+    
+
+    //// UI /////////////////////////////////////////////
 
     /// SELECT MANUALLY HOUR AND MINUTE
     $(function () {
@@ -423,8 +288,12 @@ window.onload = function () {
         },
     });
 
+    $("fieldset").hide();
     $("#toggle_manual").click(function () {
         $("fieldset").slideToggle();
+        $("#sel_hour-button").html();
+        $("#sel_hour-button").html();
+
         if (stopCloch) {
             var str = 'Stop';
             stopCloch = false;
@@ -434,7 +303,7 @@ window.onload = function () {
             stopCloch = true;
         }
         $("#startstop_cloch").html(str);
-    }); $("fieldset").hide();
+    });
     
     $("#startstop_cloch").click(function () {
         if(stopCloch) {
