3e11839db000b1e6192f43879eb7ffa35d6ccf57
sbiego <forsesim@anche.no>
2020-05-05 23:59:59 +0200

    filter hue rotation img help fixed

--- js/cloch.js
+++ js/cloch.js
@@ -193,7 +193,12 @@ function changeColor(color, duration = .8) {
 }
 
 function changeImgFilter(color) { 
-    hue = hexToHue(color) + 20 // shift because starts with magenta
+    rgb = color.substring(4, color.length - 1)
+        .replace(/ /g, '')
+        .split(',');
+         
+    console.log(rgb)
+    hue = RGBToHue(rgb) + 20 // shift because starts with magenta
     if(document.querySelector('body').classList.contains('light')) {
         hue -= 180 // for inverted colors _ shift 
         filter = 'hue-rotate('+ hue + 'deg)' + ' invert(1) contrast(1.1)'
@@ -205,54 +210,40 @@ function changeImgFilter(color) {
     })   
 }
 // used for hue rotation of images in help section, to be theme-colored too, returing only HUE
-// https: //css-tricks.com/converting-color-spaces-in-javascript/
-function hexToHue(H) {
-    // Convert hex to RGB first
-    let r = 0,
-        g = 0,
-        b = 0;
-    if (H.length == 4) {
-        r = "0x" + H[1] + H[1];
-        g = "0x" + H[2] + H[2];
-        b = "0x" + H[3] + H[3];
-    } else if (H.length == 7) {
-        r = "0x" + H[1] + H[2];
-        g = "0x" + H[3] + H[4];
-        b = "0x" + H[5] + H[6];
-    }
-    // Then to HSL
+// https://css-tricks.com/converting-color-spaces-in-javascript/
+function RGBToHue([r, g, b]) {
+    // Make r, g, and b fractions of 1
     r /= 255;
     g /= 255;
     b /= 255;
+
+    // Find greatest and smallest channel values
     let cmin = Math.min(r, g, b),
         cmax = Math.max(r, g, b),
         delta = cmax - cmin,
         h = 0//,
         // s = 0,
-        // l = 0,
-        ;
+        // l = 0;
+    
+        if (delta == 0)
+            h = 0;
+        // Red is max
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        // Green is max
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        // Blue is max
+        else
+            h = (r - g) / delta + 4;
 
-    if (delta == 0)
-        h = 0;
-    else if (cmax == r)
-        h = ((g - b) / delta) % 6;
-    else if (cmax == g)
-        h = (b - r) / delta + 2;
-    else
-        h = (r - g) / delta + 4;
+        h = Math.round(h * 60);
 
-    h = Math.round(h * 60);
+        // Make negative hues positive behind 360°
+        if (h < 0)
+            h += 360;
 
-    if (h < 0)
-        h += 360;
-
-    // l = (cmax + cmin) / 2;
-    // s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    // s = +(s * 100).toFixed(1);
-    // l = +(l * 100).toFixed(1);
-
-    // return "hsl(" + h + "," + s + "%," + l + "%)";
-    return h;
+    return h
 }
 
 function updateURL() {
